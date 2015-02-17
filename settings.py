ba5f5d153c1ffeba008dcd4a8129704e792784ec
Manu343726 <Manu343726@gmail.com>
2015-02-17 10:41:08 +0100

    generate/settings.py cli args parsing. Boost.Log working on MinGW GCC and VS2012

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ import urllib
 import json
 import sys
 import ast
+import argparse
 
 
 class BiiBoostCorruptSettingsError(Exception):
@@ -79,39 +80,56 @@ def latest_block_version(block, track):
 
 
 def settings():
-    track = sys.argv[2]
-    ci = sys.argv[3] == "ci"
-    boost_version = track if track != "master" else "1.57.0"
-    version = "STABLE"
-    publish = version if track == "master" else "disabled"
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("track", help="biicode track (Boost version) that will be generated",
+                        choices=['master', '1.57.0', '1.56.0', '1.55.0'])
+    parser.add_argument("--ci-build", "-ci", help="Specifies if the generation is being run inside a CI build", 
+                        action="store_true", dest="ci")
+    parser.add_argument("--passwords", "-pass", 
+                        help=("Dictionary containing block accounts passwords to be able to publish. "
+                              "Note that with publish disabled the passwords are not needed"),
+                        default="{}")
+    parser.add_argument("--no-publish", "-nopublish", help="Overrides publish settings and does not publish any block",
+                        action="store_true", dest="no_publish")
+    parser.add_argument("--publish-examples", help="Enables block examples publication",
+                        action="store_true", dest="publish_examples")
+    parser.add_argument("--tag", help="biicode version tag which blocks will be published",
+                        default="DEV")
+
+    args = parser.parse_args()
+
+    boost_version = args.track if args.track != "master" else "1.57.0"
+    version_tag = args.tag if not args.no_publish else "disabled"
+    examples_version_tag = args.tag if  args.publish_examples and not args.no_publish else "disabled"
+    passwords = ast.literal_eval(args.passwords.replace('->', ':'))
 
     variables = {"BIICODE_BOOST_VERSION":
                  lambda block, block_track, file: boost_version,
                  "WORKING_TRACK":
-                 lambda block, block_track, file: track,
+                 lambda block, block_track, file: args.track,
                  "BIICODE_BOOST_BLOCK":
                  lambda block, block_track, file: "biicode/boost({0})"
                                                   .format(block_track),
                  "LATEST_BLOCK_VERSION":
                  lambda block, block_track, file: latest_block_version(block, block_track)}
 
-    packages = {"biicode/boost"             : (version, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION"]), ("setup.cmake", ["BIICODE_BOOST_VERSION"])]),
-                "manu343726/math"           : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "manu343726/boost-lib"      : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "manu343726/boost-main"     : (publish, [("biicode.conf", ["LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-log"        : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-coroutine"  : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-filesystem" : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-flyweight"  : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-multiindex" : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-phoenix"    : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
-                "examples/boost-signals"    : (publish, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])])}
+    packages = {"biicode/boost"             : (version_tag,          [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION"]), ("setup.cmake", ["BIICODE_BOOST_VERSION"])]),
+                "manu343726/math"           : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "manu343726/boost-lib"      : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "manu343726/boost-main"     : (examples_version_tag, [("biicode.conf", ["LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-log"        : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-coroutine"  : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-filesystem" : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-flyweight"  : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-multiindex" : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-phoenix"    : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])]),
+                "examples/boost-signals"    : (examples_version_tag, [("biicode.conf", ["BIICODE_BOOST_BLOCK", "LATEST_BLOCK_VERSION", "WORKING_TRACK"])])}
 
     #Boost.Log takes so much time to compile, leads to timeouts on Travis CI
-    if ci: del packages['examples/boost-log']
     #It was tested on Windows and linux, works 'ok' (Be careful with linking settings)
-
-    passwords = ast.literal_eval(sys.argv[1].replace('->', ':'))
+    if args.ci: del packages['examples/boost-log']  
+    
 
     return BiiBoostSettings(packages, variables, passwords)
 
